b26f833eeb6e1fd312c767fcd1572a1c5b88b162
MishailCode <mishail34122565@gmail.com>
2025-09-22 19:09:14 +0300

    jsUpdate

--- js/index.js
+++ js/index.js
@@ -6,11 +6,92 @@
     const pet = document.querySelector('.pet');
 
     const stats = document.querySelectorAll('.stat div');
+  
+    const mStat = document.querySelector('.m-stat .stat')
+    const hStat = document.querySelector('.h-stat .stat')
     const moodStat = document.getElementById('mood_stat');
     const hungerStat = document.getElementById('hunger_stat');
     const sleepStat = document.getElementById('sleep_stat');
 
+    const apple = document.getElementById('apple')
+    const milk = document.getElementById('milk')
+    const meat = document.getElementById('meat')
+
+
     //Functions
+let food = {
+apple:15,
+milk:25,
+meat:50
+}
+
+let width = 200;
+
+
+if(width>=0){
+ setInterval(()=>{
+width--;
+hStat.style.width = width + 'px'
+console.log('-1')
+ },750)
+}
+
+function hungerUpApple(){
+if(width<200){
+width = width + food.apple
+hStat.style.width = width + 'px';
+}    
+}
+function hungerUpMilk(){
+if(width<200){
+width = width + food.milk
+hStat.style.width = width + 'px';
+}    
+}
+function hungerUpMeat(){
+if(width<200){
+width = width + food.meat
+hStat.style.width = width + 'px';
+}    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+apple.addEventListener('click',hungerUpApple)
+milk.addEventListener('click',hungerUpMilk)
+meat.addEventListener('click',hungerUpMeat)
 
     function setGame(){  //Настройка игры
         stats.forEach(value => {
