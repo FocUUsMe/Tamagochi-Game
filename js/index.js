abad4751c9873ddb4ffda1ea0a3a7ab8b7a24681
MishailCode <mishail34122565@gmail.com>
2025-09-23 15:13:00 +0300

    happiness&CD

--- js/index.js
+++ js/index.js
@@ -17,43 +17,82 @@
     const milk = document.getElementById('milk')
     const meat = document.getElementById('meat')
 
-
-    //Functions
+    const toy = document.getElementById('toy-livroom')
+//объект с едой
 let food = {
 apple:15,
 milk:25,
 meat:50
 }
+//переменные с статой
+let hunger = 200;
+let happines = 200;
 
-let width = 200;
-
-
-if(width>=0){
+//механика убавления счастья и голода
+if(hunger>=0){
  setInterval(()=>{
-width--;
-hStat.style.width = width + 'px'
+hunger--;
+hStat.style.width = hunger + 'px'
+console.log('-1')
+ },500)
+}
+if(happines>=0){
+ setInterval(()=>{
+happines--;
+mStat.style.width = happines + 'px'
 console.log('-1')
  },750)
 }
 
+
+
+ //кнопки с пополнением голода
 function hungerUpApple(){
-if(width<200){
-width = width + food.apple
-hStat.style.width = width + 'px';
+if(hunger<200){
+apple.style.display = 'none' 
+hunger = hunger + food.apple
+hStat.style.width = hunger + 'px';
+setTimeout(() => {
+apple.style.display = 'flex'
+},8700);
 }    
 }
 function hungerUpMilk(){
-if(width<200){
-width = width + food.milk
-hStat.style.width = width + 'px';
+if(hunger<200){
+milk.style.display = 'none' 
+hunger = hunger + food.milk
+hStat.style.width = hunger + 'px';
+setTimeout(() => {
+milk.style.display = 'flex'
+},12000);
 }    
 }
 function hungerUpMeat(){
-if(width<200){
-width = width + food.meat
-hStat.style.width = width + 'px';
+if(hunger<200){
+meat.style.display = 'none'  
+hunger = hunger + food.meat
+hStat.style.width = hunger + 'px';
+setTimeout(() => {
+meat.style.display = 'flex'
+},20000);
 }    
 }
+//функция пополнения счастья
+function happinesUp(){
+if(happines<200){
+toy.style.display = 'none'
+happines = happines + 10 
+mStat.style.width = happines + 'px';
+setTimeout(() => {
+toy.style.display = 'flex'
+},8000);
+}  
+}    
+if (happines>200) {
+    happines = 200;
+    mStat.style.width = happines + 'px'
+}  
+    
 
 
 
@@ -92,6 +131,7 @@ hStat.style.width = width + 'px';
 apple.addEventListener('click',hungerUpApple)
 milk.addEventListener('click',hungerUpMilk)
 meat.addEventListener('click',hungerUpMeat)
+toy.addEventListener('click',happinesUp)
 
     function setGame(){  //Настройка игры
         stats.forEach(value => {
